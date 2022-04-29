ba70990e8607b13668e70ceb7c5f5ef858e99e5e
jannatul114 <jfmithila114@gmail.com>
2022-04-29 19:03:04 +0600

    login, registar, customlink,loading component added

---
+++ src/Components/Authintications/Registar/Registart.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Registart = () => {
+    return (
+        <div>
+            <h1>registar</h1>
+        </div>
+    );
+};
+
+export default Registart;
--- src/Components/Shared/Footer/Footer.js
+++ src/Components/Shared/Footer/Footer.js
@@ -3,72 +3,23 @@ import './Footer.css';
 const Footer = () => {
     return (
         <footer className="foo text-center text-lg-start text-dark">
-            <section
-                className="d-flex justify-content-center justify-content-lg-between p-4"
-            >
-
-
-                <div>
-                    <a href="" className="me-4 text-reset">
-                        <i className="fab fa-facebook-f"></i>
-                    </a>
-                    <a href="" className="me-4 text-reset">
-                        <i className="fab fa-twitter"></i>
-                    </a>
-                    <a href="" className="me-4 text-reset">
-                        <i className="fab fa-google"></i>
-                    </a>
-                    <a href="" className="me-4 text-reset">
-                        <i className="fab fa-instagram"></i>
-                    </a>
-                    <a href="" className="me-4 text-reset">
-                        <i className="fab fa-linkedin"></i>
-                    </a>
-                    <a href="" className="me-4 text-reset">
-                        <i className="fab fa-github"></i>
-                    </a>
-                </div>
-
-            </section>
-
-
 
             <section className="">
                 <div className="container text-center text-md-start mt-5">
 
                     <div className="row mt-3">
 
-                        <div className="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
+                        <div className="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4 d-flex align-items-center">
 
-                            <h6 className="text-uppercase fw-bold mb-4">
-                                <i className="fas fa-gem me-3"></i>Company name
+                            <h6 className="fw-bold mb-4">
+                                <h1>Fruit Vally</h1>
                             </h6>
-                            <p>
-                                Here you can use rows and columns to organize your footer content. Lorem ipsum
-                                dolor sit amet, consectetur adipisicing elit.
-                            </p>
+
                         </div>
 
 
 
-                        <div className="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
 
-                            <h6 className="text-uppercase fw-bold mb-4">
-                                Products
-                            </h6>
-                            <p>
-                                <a href="#!" className="text-reset">Angular</a>
-                            </p>
-                            <p>
-                                <a href="#!" className="text-reset">React</a>
-                            </p>
-                            <p>
-                                <a href="#!" className="text-reset">Vue</a>
-                            </p>
-                            <p>
-                                <a href="#!" className="text-reset">Laravel</a>
-                            </p>
-                        </div>
 
 
 
@@ -78,16 +29,16 @@ const Footer = () => {
                                 Useful links
                             </h6>
                             <p>
-                                <a href="#!" className="text-reset">Pricing</a>
+                                <p className="text-reset">Pricing</p>
                             </p>
                             <p>
-                                <a href="#!" className="text-reset">Settings</a>
+                                <p className="text-reset">Settings</p>
                             </p>
                             <p>
-                                <a href="#!" className="text-reset">Orders</a>
+                                <p className="text-reset">Orders</p>
                             </p>
                             <p>
-                                <a href="#!" className="text-reset">Help</a>
+                                <p className="text-reset">Help</p>
                             </p>
                         </div>
 
@@ -98,13 +49,13 @@ const Footer = () => {
                             <h6 className="text-uppercase fw-bold mb-4">
                                 Contact
                             </h6>
-                            <p><i className="fas fa-home me-3"></i> New York, NY 10012, US</p>
+                            <p><i className="fas fa-home me-3"></i> Dhaka, Bangladesh</p>
                             <p>
                                 <i className="fas fa-envelope me-3"></i>
-                                info@example.com
+                                fruitsvally@gmail.com
                             </p>
-                            <p><i className="fas fa-phone me-3"></i> + 01 234 567 88</p>
-                            <p><i className="fas fa-print me-3"></i> + 01 234 567 89</p>
+                            <p><i className="fas fa-phone me-3"></i> + 01 111 222 222</p>
+                            <p><i className="fas fa-print me-3"></i> + 01 333 666 444</p>
                         </div>
 
                     </div>
